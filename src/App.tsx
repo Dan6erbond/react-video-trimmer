8ffd6c7b4bc71a10c4ddb17cc344090bb7df46f5
RaviAnand Mohabir <moravrav@gmail.com>
2023-04-26 14:48:41 +0200

    feat: :sparkles: implement `VideoTrimmer` in main app for preview and only display trimmer if video URL is set

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 import { Box, TextInput } from "@mantine/core";
 import { useState } from "react";
 import HlsVideoTrimmer from "./HlsVideoTrimmer";
+import VideoTrimmer from "./VideoTrimmer";
+
+interface XMLHttpRequestWithMethod extends XMLHttpRequest {
+  method: string;
+}
 
 function App() {
   const [hlsLiveStreamUrl, setHlsLiveStreamUrl] = useState("");
+  const [streamUrl, setStreamUrl] = useState("");
 
   return (
     <Box component="main" p="md">
       <TextInput
-        label="HLS Stream"
+        label="HLS Stream URL"
         value={hlsLiveStreamUrl}
         onChange={(e) => setHlsLiveStreamUrl(e.target.value)}
         mb="md"
       />
-      <HlsVideoTrimmer hlsLiveStreamUrl={hlsLiveStreamUrl} />
+      {hlsLiveStreamUrl && (
+        <HlsVideoTrimmer hlsLiveStreamUrl={hlsLiveStreamUrl} />
+      )}
+      <TextInput
+        label="Stream URL"
+        value={streamUrl}
+        onChange={(e) => setStreamUrl(e.target.value)}
+        mb="md"
+      />
+      {streamUrl && <VideoTrimmer streamUrl={streamUrl} />}
     </Box>
   );
 }
